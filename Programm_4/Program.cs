d3401a2c4786cb5b82c66865461f2a56f6b66261
Andrey <acord.81@mail.ru>
2022-07-13 20:58:25 +0300

    Создали четвертую программу

---
+++ Programm_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите положительное целое число");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
