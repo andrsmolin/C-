4d4afaf0d53cee2f175378ab330b2e16b9cad83c
Andrey <acord.81@mail.ru>
2022-07-13 20:17:19 +0300

    Составили трерью программу

--- Programm_3/Program.cs
+++ Programm_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Введенное число {a} - четное");
+}
+else
+{
+    Console.WriteLine($"Введенное число {a} - нечетное");
+}
