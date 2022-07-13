82f410cbe64c265b847bc788b2ed5407c3ed93b1
Andrey <acord.81@mail.ru>
2022-07-13 08:58:39 +0300

    Вторая программа в ветке max3

--- Programm_2/Program.cs
+++ Programm_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int e = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a < b)
+{
+    if (b > e)
+    {
+        max = b;
+    }
+    else
+    {
+        max = e;
+    }
+}
+Console.WriteLine($"Максимальное число - {max}");
