dc2c1d29a3ea7fe060ba44e83829009880e74590
Andrey <acord.81@mail.ru>
2022-07-14 18:19:49 +0300

    Программа находит 3е число

---
+++ Zadanie_13/Program.cs
@@ -0,0 +1,31 @@
+﻿void Number(int number)
+{
+    if (number >= 100)
+    {
+        if (number < 1000)
+        {
+            int ones = number % 10;
+            Console.WriteLine(ones);
+        }
+        else if (number < 10000)
+        {
+            int tens = number / 10 % 10;
+            Console.WriteLine(tens);
+        }
+        else
+        {
+            int hundreds = number / 100 % 10;
+            Console.WriteLine(hundreds);
+        }
+    }
+    else
+    {
+        Console.WriteLine("У этой цифры нет третьего чиcла");
+    }
+}
+
+Random rand = new Random();
+int num = rand.Next(1, 100000);
+Console.WriteLine(num);
+Number(num);
+
