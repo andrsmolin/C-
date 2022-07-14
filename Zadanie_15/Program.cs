62db5455be9616b15c01279c6bb30de2aa4120d5
Andrey <acord.81@mail.ru>
2022-07-14 19:13:53 +0300

    Вычисляем выходной

---
+++ Zadanie_15/Program.cs
@@ -0,0 +1,20 @@
+﻿void Weekend(int i)
+{
+    string[] week = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+    if (i < 6)
+    {
+
+        Console.WriteLine($"{week[i - 1]} не выходной");
+    }
+    else if (i < 8)
+    {
+        Console.WriteLine($"{week[i - 1]} - выходной!!!");
+    }
+    else
+    { Console.WriteLine("Проверьте правильность написания числа"); }
+
+}
+
+Console.WriteLine("Введите число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+Weekend(a);
