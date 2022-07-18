5d80269d0cb69e8d6e64981fea322798fa8e1179
Andrey <acord.81@mail.ru>
2022-07-18 20:34:49 +0300

    Вычисляем палиндром

---
+++ Zadanie_19/Program.cs
@@ -0,0 +1,23 @@
+﻿void Abcba(int number)
+{
+    int[] array = new int[5];
+    int power = 1;
+    for (int i = 0; i < 5; i++)
+    {
+        array[i] = number / power % 10;
+        power *= 10;
+    }
+    if (array[0] == array[4] && array[1] == array[3])
+    {
+        Console.WriteLine($"Введенное число {number} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Введенное число {number} - не палиндром");
+    }
+}
+
+Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+Abcba(a);
+
