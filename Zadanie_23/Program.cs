1cba5d42409338fbed81d34f92e1755923f8182c
Andrey <acord.81@mail.ru>
2022-07-20 10:33:01 +0300

    Программа посмтроения ряда кубов

---
+++ Zadanie_23/Program.cs
@@ -0,0 +1,12 @@
+﻿void Cube()
+{
+    Console.WriteLine("Введите число для построение ряда");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1; i <= x; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " ");
+    }
+}
+
+Cube();
